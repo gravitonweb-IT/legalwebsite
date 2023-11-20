e053701bc828efc3ce77b46cc3d1bf3d2e1c3c5f
lokesh2518 <lokesh.tiwari@gravitonweb.com>
2023-11-20 15:08:48 +0530

    how the josh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <div>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="grid grid-cols-1  md:grid-cols-2   gap-10 mx-10 md:mx-20 lg:mx-32 mt-5">
+     
+     <div className="font-bold text-2xl ">
+            <Typewriter
+              options={{
+                strings: [
+                  "One-Stop Solution to All  Your  Legal Problems",
+                  "Get Your Personal Legal Advisor ",
+                  "Now Legal is Always On  Your Phone",
+                ],
+                autoStart: true,
+                loop: true,
+                typeSpeed: 50,
+              }}
+            />
+          </div>
+     
+
+<div>
+hello
+</div>
+
+
 
     </div>
+    </>
   )
 }
 
--- src/components/StartBusiness/ParternLtdCompanyRegisteration.jsx
+++ src/components/StartBusiness/ParternLtdCompanyRegisteration.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ParternLtdCompanyRegisteration = () => {
   return (
-    <div>ParternLtdCompanyRegisteration</div>
+    <div>Pvt. Ltd. Company Registration</div>
   )
 }
 
